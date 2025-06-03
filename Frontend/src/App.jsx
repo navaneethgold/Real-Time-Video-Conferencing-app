816f7a3f982cf0fca4742cfc478666fa40cd2125
navaneethgold <navaneethabs.2006@gmail.com>
2025-06-03 20:40:40 +0530

    updated app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,13 +8,16 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Chatting from './pages/chatting';
 import MeetVideo from './pages/videoConference';
+import TopBar from './boilerPlate/topBar';
+import History from './pages/history';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
           <Route path='/login' element={<Login/>}/>
+          <Route path='/history' element={<History/>}/>
+          <Route path="/home" element={<Home />} />
           <Route path='/chatting' element={<Chatting/>}/>
           <Route path='/videoConference' element={<MeetVideo/>}/>
       </Routes>
---
+++ Frontend/src/pages/history.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+
+const History=()=>{
+
+}
+export default History;
